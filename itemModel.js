97d75db5a3238dcda214daa3ff5dda4c9b353b1b
Abdessamad <abdsamad23bnsaad@gmail.com>
2020-12-03 10:28:55 +0100

    query search

--- itemModel.js
+++ itemModel.js
@@ -1,28 +1,39 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 // ! Setup schema
 var datalistSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        require: true
-    },
+  name: {
     type: String,
-    imgUrl: {
-        type: String,
-        require: true
-    },
-    rate: {
-        type: String,
-        require: true
-    },
-    /*
+    required: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  imgUrl: {
+    type: String,
+    require: true,
+  },
+  rate: {
+    type: String,
+    require: true,
+  },
+  category: String,
+  create_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+// ! Export Contact model
+var DataList = (module.exports = mongoose.model("datalists", datalistSchema));
+module.exports.get = function (callback, limit) {
+  DataList.find(callback).limit(limit);
+};
+
+/*
      My rating chart will be like this:
       1  ==> Very Boring
       2  ==> Not so dusty!
@@ -30,13 +41,3 @@ var datalistSchema = mongoose.Schema({
       3  ==> Very cool
       5  ==> Just WOW
     */
-    create_date: {
-        type: Date,
-        default: Date.now
-    }
-});
-// ! Export Contact model
-var DataList = module.exports = mongoose.model('datalists', datalistSchema);
-module.exports.get = function (callback, limit) {
-    DataList.find(callback).limit(limit);
-}
